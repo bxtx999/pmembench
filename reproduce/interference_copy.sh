d0fbf297ae793603d6626c8796e7568de879cd9d
bxtx999 <fred1653@qq.com>
2021-04-27 14:24:02 +0800

    update setttings

--- reproduce/interference_copy.sh
+++ reproduce/interference_copy.sh
@@ -9,112 +9,112 @@ clang++ -g0 -O3 -DNDEBUG=1 -march=native -std=c++17 interference/interference.cp
 
 # Seqential ram
 echo "" > results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  1  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  4  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  8  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  1  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  4  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  8  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  0  1  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  0  4  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  0  8  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  0  0  1  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  0  0  4  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  0  0  8  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  0  0  0  1 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  0  0  0  4 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
-numactl --cpunodebind=0 ./a.out 8  0  0  0  0  8 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  1  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  5  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  10  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  1  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  5  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  10  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  0  1  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  0  5  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  0  10  0  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  0  0  1  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  0  0  5  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  0  0  10  0 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  0  0  0  1 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  0  0  0  5 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
+numactl --cpunodebind=0 ./a.out 10  0  0  0  0  10 /mnt/pmem/renen | tee -a results/interference_seq_ram.txt
 
 # Seqential nvm
 echo "" > results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  1 8  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  4 8  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  8 8  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  1  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  4  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  8  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  0  1  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  0  4  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  0  8  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  0  0  1  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  0  0  4  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  0  0  8  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  0  0  0  1 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  0  0  0  4 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
-numactl --cpunodebind=0 ./a.out  0 8  0  0  0  8 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  1 10  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  5 10  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  10 10  0  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  1  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  5  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  10  0  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  0  1  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  0  5  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  0  10  0  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  0  0  1  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  0  0  5  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  0  0  10  0 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  0  0  0  1 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  0  0  0  5 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
+numactl --cpunodebind=0 ./a.out  0 10  0  0  0  10 /mnt/pmem/renen | tee -a results/interference_seq_nvm.txt
 
 # Random ram
 echo "" > results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  0 8  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  1  0 8  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  4  0 8  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  8  0 8  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  1 8  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  4 8  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  8 8  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  0 8  1  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  0 8  4  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  0 8  8  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  0 8  0  1  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  0 8  0  4  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  0 8  0  8  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  0 8  0  0  1 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  0 8  0  0  4 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
-numactl --cpunodebind=0 ./a.out  0  0 8  0  0  8 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  0 10  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  1  0 10  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  5  0 10  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  10  0 10  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  1 10  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  5 10  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  10 10  0  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  0 10  1  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  0 10  5  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  0 10  10  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  0 10  0  1  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  0 10  0  5  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  0 10  0  10  0 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  0 10  0  0  1 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  0 10  0  0  5 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
+numactl --cpunodebind=0 ./a.out  0  0 10  0  0  10 /mnt/pmem/renen | tee -a results/interference_rnd_ram.txt
 
 # Random nvm
 echo "" > results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  0  0 8  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  1  0  0 8  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  4  0  0 8  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  8  0  0 8  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  1  0 8  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  4  0 8  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  8  0 8  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  0  1 8  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  0  4 8  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  0  8 8  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  0  0 8  1  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  0  0 8  4  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  0  0 8  8  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  0  0 8  0  1 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  0  0 8  0  4 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
-numactl --cpunodebind=0 ./a.out  0  0  0 8  0  8 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  0  0 10  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  1  0  0 10  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  5  0  0 10  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  10  0  0 10  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  1  0 10  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  5  0 10  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  10  0 10  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  0  1 10  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  0  5 10  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  0  10 10  0  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  0  0 10  1  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  0  0 10  5  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  0  0 10  10  0 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  0  0 10  0  1 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  0  0 10  0  5 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
+numactl --cpunodebind=0 ./a.out  0  0  0 10  0  10 /mnt/pmem/renen | tee -a results/interference_rnd_nvm.txt
 
 ## Log nvm
-#./a.out  0  0  0  0 8  0 /mnt/pmem/renen
-#./a.out  1  0  0  0 8  0 /mnt/pmem/renen
-#./a.out  4  0  0  0 8  0 /mnt/pmem/renen
-#./a.out  8  0  0  0 8  0 /mnt/pmem/renen
-#./a.out  0  1  0  0 8  0 /mnt/pmem/renen
-#./a.out  0  4  0  0 8  0 /mnt/pmem/renen
-#./a.out  0  8  0  0 8  0 /mnt/pmem/renen
-#./a.out  0  0  1  0 8  0 /mnt/pmem/renen
-#./a.out  0  0  4  0 8  0 /mnt/pmem/renen
-#./a.out  0  0  8  0 8  0 /mnt/pmem/renen
-#./a.out  0  0  0  1 8  0 /mnt/pmem/renen
-#./a.out  0  0  0  4 8  0 /mnt/pmem/renen
-#./a.out  0  0  0  8 8  0 /mnt/pmem/renen
-#./a.out  0  0  0  0 8  1 /mnt/pmem/renen
-#./a.out  0  0  0  0 8  4 /mnt/pmem/renen
-#./a.out  0  0  0  0 8  8 /mnt/pmem/renen
+#./a.out  0  0  0  0 10  0 /mnt/pmem/renen
+#./a.out  1  0  0  0 10  0 /mnt/pmem/renen
+#./a.out  5  0  0  0 10  0 /mnt/pmem/renen
+#./a.out  10  0  0  0 10  0 /mnt/pmem/renen
+#./a.out  0  1  0  0 10  0 /mnt/pmem/renen
+#./a.out  0  5  0  0 10  0 /mnt/pmem/renen
+#./a.out  0  10  0  0 10  0 /mnt/pmem/renen
+#./a.out  0  0  1  0 10  0 /mnt/pmem/renen
+#./a.out  0  0  5  0 10  0 /mnt/pmem/renen
+#./a.out  0  0  10  0 10  0 /mnt/pmem/renen
+#./a.out  0  0  0  1 10  0 /mnt/pmem/renen
+#./a.out  0  0  0  5 10  0 /mnt/pmem/renen
+#./a.out  0  0  0  10 10  0 /mnt/pmem/renen
+#./a.out  0  0  0  0 10  1 /mnt/pmem/renen
+#./a.out  0  0  0  0 10  5 /mnt/pmem/renen
+#./a.out  0  0  0  0 10  10 /mnt/pmem/renen
 #
 ## Page nvm
-# ./a.out  0  0  0  0  0 8 /mnt/pmem/renen
-# ./a.out  1  0  0  0  0 8 /mnt/pmem/renen
-# ./a.out  4  0  0  0  0 8 /mnt/pmem/renen
-# ./a.out  8  0  0  0  0 8 /mnt/pmem/renen
-# ./a.out  0  1  0  0  0 8 /mnt/pmem/renen
-# ./a.out  0  4  0  0  0 8 /mnt/pmem/renen
-# ./a.out  0  8  0  0  0 8 /mnt/pmem/renen
-# ./a.out  0  0  1  0  0 8 /mnt/pmem/renen
-# ./a.out  0  0  4  0  0 8 /mnt/pmem/renen
-# ./a.out  0  0  8  0  0 8 /mnt/pmem/renen
-# ./a.out  0  0  0  1  0 8 /mnt/pmem/renen
-# ./a.out  0  0  0  4  0 8 /mnt/pmem/renen
-# ./a.out  0  0  0  8  0 8 /mnt/pmem/renen
-# ./a.out  0  0  0  0  1 8 /mnt/pmem/renen
-# ./a.out  0  0  0  0  4 8 /mnt/pmem/renen
-# ./a.out  0  0  0  0  8 8 /mnt/pmem/renen
+# ./a.out  0  0  0  0  0 10 /mnt/pmem/renen
+# ./a.out  1  0  0  0  0 10 /mnt/pmem/renen
+# ./a.out  5  0  0  0  0 10 /mnt/pmem/renen
+# ./a.out  10  0  0  0  0 10 /mnt/pmem/renen
+# ./a.out  0  1  0  0  0 10 /mnt/pmem/renen
+# ./a.out  0  5  0  0  0 10 /mnt/pmem/renen
+# ./a.out  0  10  0  0  0 10 /mnt/pmem/renen
+# ./a.out  0  0  1  0  0 10 /mnt/pmem/renen
+# ./a.out  0  0  5  0  0 10 /mnt/pmem/renen
+# ./a.out  0  0  10  0  0 10 /mnt/pmem/renen
+# ./a.out  0  0  0  1  0 10 /mnt/pmem/renen
+# ./a.out  0  0  0  5  0 10 /mnt/pmem/renen
+# ./a.out  0  0  0  10  0 10 /mnt/pmem/renen
+# ./a.out  0  0  0  0  1 10 /mnt/pmem/renen
+# ./a.out  0  0  0  0  5 10 /mnt/pmem/renen
+# ./a.out  0  0  0  0  10 10 /mnt/pmem/renen
